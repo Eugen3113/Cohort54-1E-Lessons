3a1b97ee88c5ee58f2ade06b808e94d993ac32a9
Eugen <eg31it@gmail.com>
2024-10-29 09:28:37 +0100

    add

--- src/lesson_02/TestTest0.java
+++ src/lesson_02/TestTest0.java
@@ -3,5 +3,7 @@ package lesson_02;
 public class TestTest0 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Auto");
+        System.out.println("Truck");
     }
 }
